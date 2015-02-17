2342de20a8d253d0478a25daa19fa160ff32261b
jlu19 <jlu19@illinois.edu>
2015-02-17 14:21:18 -0600

    version1

--- js/script.js
+++ js/script.js
@@ -1,16 +1,52 @@
-var divs = document.getElementsByTagName('div');
-for(var i=0; i<divs.length; i++) {
-  divs[i].addEventListener("click", highlightThis);
-  /*
-  divs[i].addEventListener("click", highlightThis, true);
-  divs[i].addEventListener("click", highlightThis, false);*/
-}
-
-function highlightThis(event) {
-    //event.stopPropagation();
+$(document).ready(function() {  
+var stickyNavTop = $(".nav").offset().top;  
   
-    var backgroundColor = this.style.backgroundColor;
-    this.style.backgroundColor='yellow';
-    alert(this.className);
-    this.style.backgroundColor=backgroundColor;
+var stickyNavbar = function(){  
+var scrollTop = $(window).scrollTop();  
+       
+if (scrollTop > stickyNavTop) 
+{   
+    $(".nav").addClass("stickyNav");
 }
+else 
+{  
+    $(".nav").removeClass("stickyNav"); 
+}
+};  
+   
+stickyNavbar();  
+  
+$(window).scroll(function() {  
+    stickyNavbar();  
+});  
+});  
+
+$(document).ready(function(){
+  $('a[href*=#]').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    && location.hostname == this.hostname) {
+      var $target = $(this.hash);
+      $target = $target.length && $target
+      || $('[name=' + this.hash.slice(1) +']');
+      if ($target.length) {
+        var targetOffset = $target.offset().top;
+        $('html,body')
+        .animate({scrollTop: targetOffset}, 1000);
+       return false;
+      }
+    }
+  });
+});
+
+
+$(document).ready(function(){
+$('.bxslider').bxSlider({
+  mode: 'horizontal',
+  useCSS: false,
+  infiniteLoop: false,
+  hideControlOnEnd: true,
+  easing: 'easeOutElastic',
+  speed: 2000
+});
+});
+
