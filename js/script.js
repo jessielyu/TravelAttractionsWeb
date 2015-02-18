84c2f32173fcf74fc2ab4f2ac966744ffd89d647
jlu19 <jlu19@illinois.edu>
2015-02-18 16:46:23 -0600

    version7

--- js/script.js
+++ js/script.js
@@ -28,15 +28,26 @@ $(window).scroll(function() {
     	$(".nav").removeClass("smallerNav");
     	$(".nav ul li a").removeClass("smallerNav ul li a");
     }
-
-    console.log($(".nav ul li a#TW"));
-    if (top>200 && top<700) {
+    console.log(top);
+    if (top>358 && top<1002) {
     	$(".nav ul li a").removeClass("highlight");
     	$(".nav ul li a#TW").addClass("highlight");
     }
-    else if (top>700 && top<900) {
+    else if (top>1002 && top<1178) {
     	$(".nav ul li a").removeClass("highlight");
-    	$(".nav ul li a#TW").addClass("highlight");
+    	$(".nav ul li a#HI").addClass("highlight");
+    }
+    else if (top>1178 && top<1777) {
+        $(".nav ul li a").removeClass("highlight");
+        $(".nav ul li a#ORD").addClass("highlight");
+    }
+    else if (top>1777 && top<2340) {
+        $(".nav ul li a").removeClass("highlight");
+        $(".nav ul li a#CHI").addClass("highlight");
+    }
+    else if (top>2340 && top<2387) {
+        $(".nav ul li a").removeClass("highlight");
+        $(".nav ul li a#LAX").addClass("highlight");
     }
     else 
     {
@@ -110,4 +121,54 @@ $(document).ready(function ($) {
     });
 
     var a = $('a.control_next');
+});
+
+
+
+//modal window function
+$(document).ready(function() {    
+    //select all the a tag with name equal to modal
+    $('a[name=modal]').click(function(e) {
+        //Cancel the link behavior
+        e.preventDefault();
+        //Get the A tag
+        var id = $(this).attr('href');
+    
+        //Get the screen height and width
+        var maskHeight = $(document).height();
+        var maskWidth = $(window).width();
+    
+        //Set height and width to mask to fill up the whole screen
+        $('#mask').css({'width':maskWidth,'height':maskHeight});
+        
+        //transition effect        
+        $('#mask').fadeIn(1000);    
+        $('#mask').fadeTo("slow",0.8);    
+    
+        //Get the window height and width
+        var winH = $(window).height();
+        var winW = $(window).width();
+              
+        //Set the popup window to center
+        $(id).css('top',  winH/2-$(id).height()/2);
+        $(id).css('left', winW/2-$(id).width()/2);
+    
+        //transition effect
+        $(id).fadeIn(2000); 
+    
+    });
+    
+    //if close button is clicked
+    $('.window .close').click(function (e) {
+        //Cancel the link behavior
+        e.preventDefault();
+        $('#mask, .window').hide();
+    });        
+    
+    //if mask is clicked
+    $('#mask').click(function () {
+        $(this).hide();
+        $('.window').hide();
+    });            
+    
 });
