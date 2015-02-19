3606aa931fb3364a53de2fa10bfb67eb0da012de
jlu19 <jlu19@illinois.edu>
2015-02-18 22:46:08 -0600

    !

--- js/script.js
+++ js/script.js
@@ -29,23 +29,23 @@ $(window).scroll(function() {
     	$(".nav ul li a").removeClass("smallerNav ul li a");
     }
     console.log(top);
-    if (top>358 && top<1002) {
+    if (top>358 && top<1190) {
     	$(".nav ul li a").removeClass("highlight");
     	$(".nav ul li a#TW").addClass("highlight");
     }
-    else if (top>1002 && top<1178) {
+    else if (top>1190 && top<1567) {
     	$(".nav ul li a").removeClass("highlight");
     	$(".nav ul li a#HI").addClass("highlight");
     }
-    else if (top>1178 && top<1777) {
+    else if (top>1567 && top<2368) {
         $(".nav ul li a").removeClass("highlight");
         $(".nav ul li a#ORD").addClass("highlight");
     }
-    else if (top>1777 && top<2340) {
+    else if (top>2368 && top<3439) {
         $(".nav ul li a").removeClass("highlight");
         $(".nav ul li a#CHI").addClass("highlight");
     }
-    else if (top>2340 && top<2387) {
+    else if (top>3439 && top<4000) {
         $(".nav ul li a").removeClass("highlight");
         $(".nav ul li a#LAX").addClass("highlight");
     }
@@ -126,11 +126,22 @@ $(document).ready(function ($) {
 var flag = true;
 function resize() {
     if(flag) {
+        document.getElementById("cuteicon").style.width = "4%";
+        document.getElementById("cuteicon").style.height = "4%";
+    } else {
         document.getElementById("cuteicon").style.width = "10%";
         document.getElementById("cuteicon").style.height = "10%";
-    } else {
-        document.getElementById("cuteicon").style.width = "20%";
-        document.getElementById("cuteicon").style.height = "20%";
     }
     (flag)?flag=false:flag=true;
 } 
+
+function modal() {
+        document.getElementById("window").style.display = "block";
+        console.log("Jessiehaha");
+}
+
+function close() {
+        document.getElementById("window").style.display = "none";
+        console.log("Jessie");
+
+}
